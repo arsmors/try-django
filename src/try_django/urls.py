e57f5e5784aff7d0659a47539d66f055e14498ef
Morins <797505@cognizant.com>
2019-09-03 14:17:44 +0300

    6 commit

--- src/try_django/urls.py
+++ src/try_django/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
-from blog.views import blog_post_detail_page, blog_post_list_view
+from blog.views import blog_post_detail_view, blog_post_list_view, blog_post_create_view, blog_post_update_view, blog_post_delete_view
 from .views import home_page, about_page, contact_page, example_page
 
 urlpatterns = [
@@ -26,7 +26,10 @@ urlpatterns = [
     path('contact/', contact_page),
     path('admin/', admin.site.urls),
     path('blog/', blog_post_list_view),
-    path('blog/<str:slug>', blog_post_detail_page),
+    path('blog/<str:slug>/', blog_post_detail_view),
+    path('blog/<str:slug>/edit', blog_post_update_view),
+    path('blog/<str:slug>/delete', blog_post_delete_view),
+    path('blog-new', blog_post_create_view),
     #re_path(r'^blog/(?P<post_id>\d+)/$', blog_post_detail_page)
 
 
