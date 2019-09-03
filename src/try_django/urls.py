3c51d90d164aca602c9959845933358b39ec5481
Morins <797505@cognizant.com>
2019-09-03 12:09:49 +0300

    3 commit

--- src/try_django/urls.py
+++ src/try_django/urls.py
@@ -15,14 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
-
-from .views import home_page, about_page, contact_page
+from blog.views import blog_post_detail_page
+from .views import home_page, about_page, contact_page, example_page
 
 urlpatterns = [
     path('', home_page),
     re_path(r'^pages?/$', about_page),
     re_path(r'^about/$', about_page),
+    path('example/', example_page),
     path('contact/', contact_page),
     path('admin/', admin.site.urls),
+    path('blog/', blog_post_detail_page)
+
+
 
 ]
