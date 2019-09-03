f7e76c0804756a3a360c0a8bff1cb81f0f48fe3b
Morins <797505@cognizant.com>
2019-09-03 15:30:19 +0300

    7 commit

--- src/try_django/urls.py
+++ src/try_django/urls.py
@@ -14,22 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
-from blog.views import blog_post_detail_view, blog_post_list_view, blog_post_create_view, blog_post_update_view, blog_post_delete_view
+from django.urls import path, re_path, include
+from blog.views import (
+    blog_post_create_view,
+)
 from .views import home_page, about_page, contact_page, example_page
 
 urlpatterns = [
     path('', home_page),
+    path('blog-new', blog_post_create_view),
+    path('blog/', include('blog.urls')),
     re_path(r'^pages?/$', about_page),
     re_path(r'^about/$', about_page),
     path('example/', example_page),
     path('contact/', contact_page),
     path('admin/', admin.site.urls),
-    path('blog/', blog_post_list_view),
-    path('blog/<str:slug>/', blog_post_detail_view),
-    path('blog/<str:slug>/edit', blog_post_update_view),
-    path('blog/<str:slug>/delete', blog_post_delete_view),
-    path('blog-new', blog_post_create_view),
     #re_path(r'^blog/(?P<post_id>\d+)/$', blog_post_detail_page)
 
 
