4ca07e879abfa39ff3b9b50f900502c158223012
Morins <797505@cognizant.com>
2019-09-04 14:13:16 +0300

    8

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,46 +1,59 @@
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404
-from django.shortcuts import Http404
+
 # Create your views here.
+from .forms import BlogPostForm
 from .models import BlogPost
 
+# CRUD
 
+# GET -> Retrieve / List
 
-# def blog_post_detail_page(request, slug):
-#     # queryset = BlogPost.objects.filter(slug=slug)
-#     # if queryset.count() == 0:
-#     #     raise Http404
-#     # obj = queryset.first()
-#     obj = get_object_or_404(BlogPost, slug=slug)
-#     template_name = 'blog_post_detail.html'
-#     context = {"object":obj}
-#     return render(request, template_name, context)
+# POST -> Create / Update / DELETE
+
+# Create Retrieve Update Delete
+
 
 
 def blog_post_list_view(request):
-    qs = BlogPost.objects.all()
-    template_name = 'blog_post_list.html'
+    # list out objects
+    # could be search
+    qs = BlogPost.objects.all() # queryset -> list of python object
+    template_name = 'blog/list.html'
     context = {'object_list': qs}
     return render(request, template_name, context)
 
+
+
 def blog_post_create_view(request):
-    template_name = 'blog_post_create.html'
-    context = {'form': None}
+    # create objects
+    # ? use a form
+    form = BlogPostForm(request.POST or None)
+    if form.is_valid():
+        obj = BlogPost.objects.create(**form.cleaned_data)
+        form = BlogPostForm()
+    template_name = 'form.html'
+    context = {'form': form}
     return render(request, template_name, context)
 
+
 def blog_post_detail_view(request, slug):
+    # 1 object -> detail view
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog_post_detail.html'
+    template_name = 'blog/detail.html'
     context = {"object": obj}
     return render(request, template_name, context)
 
-def blog_post_update_view(request, slug):
+
+def blog_post_update_view(request):
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog_post_update.html'
+    template_name = 'blog/update.html'
     context = {"object": obj, 'form': None}
     return render(request, template_name, context)
 
-def blog_post_delete_view(request, slug):
+
+def blog_post_delete_view(request):
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog_post_delete.html'
+    template_name = 'blog/delete.html'
     context = {"object": obj}
     return render(request, template_name, context)
--- src/try_django/urls.py
+++ src/try_django/urls.py
@@ -13,24 +13,32 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path, re_path, include # url
 from blog.views import (
     blog_post_create_view,
 )
-from .views import home_page, about_page, contact_page, example_page
+from .views import (
+    home_page,
+    about_page,
+    contact_page,
+    example_page
+)
+
 
 urlpatterns = [
     path('', home_page),
-    path('blog-new', blog_post_create_view),
+
+    path('blog-new/', blog_post_create_view),
     path('blog/', include('blog.urls')),
+
+    # re_path(r'^blog/(?P<slug>\w+)/$', blog_post_detail_view),
+    path('page/', about_page),
+    path('pages/', about_page),
     re_path(r'^pages?/$', about_page),
     re_path(r'^about/$', about_page),
     path('example/', example_page),
     path('contact/', contact_page),
     path('admin/', admin.site.urls),
-    #re_path(r'^blog/(?P<post_id>\d+)/$', blog_post_detail_page)
-
-
-
 ]
