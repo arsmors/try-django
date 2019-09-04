a44903092ee34f0b56b4868ee9ba7401e1e9529e
Morins <797505@cognizant.com>
2019-09-04 15:34:27 +0300

    9

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
 class BlogPost(models.Model):
-    title = models.TextField()
-    slug = models.SlugField(unique=True)
-    content = models.TextField(null=True, blank=True)
-
-
-
+    # id = models.IntegerField() # pk
+    title  = models.CharField(max_length=120)
+    slug   = models.SlugField(unique=True) # hello world -> hello-world
+    content  = models.TextField(null=True, blank=True)
