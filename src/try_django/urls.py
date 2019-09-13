b32c87a3dde5f96855c6793792425d8c31867353
arsens <arsens@arsenss-MacBook-Pro.local>
2019-09-13 20:20:12 +0300

    friday2

--- src/try_django/urls.py
+++ src/try_django/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, re_path, include # url
 from blog.views import (
@@ -42,3 +43,8 @@ urlpatterns = [
     path('contact/', contact_page),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
