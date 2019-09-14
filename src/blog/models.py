74305529644234c289e4d90c8ffd15a29fba1778
arsens <arsens@arsenss-MacBook-Pro.local>
2019-09-14 10:58:42 +0300

    fr6

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+from django.db.models import Q
+
 # Create your models here.
 
 User = settings.AUTH_USER_MODEL
+
 
 class BlogPostQuerySet(models.QuerySet):
     def published(self):
@@ -11,7 +14,17 @@ class BlogPostQuerySet(models.QuerySet):
         return self.filter(publish_date__lte=now)
 
     def search(self, query):
-        return self.filter(title__iexact=query)
+        lookup = (
+                    Q(title__icontains=query) |
+                    Q(content__icontains=query) |
+                    Q(slug__icontains=query) |
+                    Q(user__first_name__icontains=query) |
+                    Q(user__last_name__icontains=query) |
+                    Q(user__username__icontains=query)
+                    )
+
+        return self.filter(lookup)
+
 
 class BlogPostManager(models.Manager):
     def get_queryset(self):
@@ -26,14 +39,13 @@ class BlogPostManager(models.Manager):
         return self.get_queryset().published().search(query)
 
 
-
 class BlogPost(models.Model):
     # id = models.IntegerField() # pk
     user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
     image = models.ImageField(upload_to='image/', blank=True, null=True)
-    title  = models.CharField(max_length=120)
-    slug   = models.SlugField(unique=True) # hello world -> hello-world
-    content  = models.TextField(null=True, blank=True)
+    title = models.CharField(max_length=120)
+    slug = models.SlugField(unique=True)  # hello world -> hello-world
+    content = models.TextField(null=True, blank=True)
     publish_date = models.DateTimeField(auto_now=False, auto_now_add=False, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
