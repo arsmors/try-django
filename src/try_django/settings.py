b567b1d5be43c55b0a73125cf01e128a85e3d4e2
arsens <arsens@arsenss-MacBook-Pro.local>
2019-09-15 11:05:56 +0300

    db10

--- src/try_django/settings.py
+++ src/try_django/settings.py
@@ -142,6 +142,8 @@ MEDIA_URL = '/media/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 import dj_database_url
-db_from_env = dj_database_url.config(default='postgres://localhost')
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+# db_from_env = dj_database_url.config(default='postgres://localhost')
+# DATABASES['default'].update(db_from_env)
 #DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
